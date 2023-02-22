f3c4e145d47519eea2b74308f59692b35eab52da
tawakuliKH <tawakuli456@gmail.com>
2023-02-23 00:48:30 +0430

    Update app.js file for linters errors

--- app.js
+++ app.js
@@ -9,16 +9,14 @@ class BookCollect {
     window.location.reload();
   }
 
-    addBook(BookName, Author) {
+  addBook(BookName, Author) {
     const book = {
       name: BookName,
       author: Author,
     };
-    
     this.books.push(book);
     localStorage.setItem('Books', JSON.stringify(this.books));
   }
-  
 }
 
 const BookCollection = new BookCollect();
